0783f56656eb3d6aae4184fe162fa1808f1292a7
mrchappell <michael.r.chappell@gmail.com>
2020-01-02 20:10:00 -0600

    Factorial

---
+++ Day1-Functions.js
@@ -0,0 +1,9 @@
+function factorial(n){
+    if(n === 0){
+        return 1;
+    } else{
+        return n * factorial(n-1);
+    }
+}
+
+//Task Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
